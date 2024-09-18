2a47396814596da8f3908dd10705fcbc4a542935
chenna1584 <chennakesavareddy0205@gmail.com>
2024-09-18 17:57:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DOM-manipulation';
+  @ViewChild("test") d:any
+
+ngAfterViewInit(){
+  console.log(this.d)
+ this.d.nativeElement.onkeyup = ()=>{
+  console.log(this.d.nativeElement.value)
+ }
+}
+
+changeDiv(){
+  console.log(this.d.nativeElement.value)
+ }
+  
+  
+
+  
+
 }
